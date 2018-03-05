0be0c58339bbf7620e3d3953aa06dc31c9ceb804
Yossif Nassar <ynassar@outbrain.com>
2018-03-05 17:12:19 +0200

    edited run bash script

--- run.bash
+++ run.bash
@@ -3,9 +3,9 @@ echo compressing kibana logtrail plugin...
 zip -r logtrail.zip  kibana
 echo done.
 echo building docker image...
-docker build -t "docker-kibana5:dockerfile" .
+#docker build -t "docker-kibana5:dockerfile" .
 echo done. 
 echo running kibana container...
-docker rm -f docker-kibana5:dockerfile
+docker ps -a -q --filter ancestor=docker-kibana5:dockerfile | xargs docker rm -f
 docker run -d -e ELASTICSEARCH_URL=http://elasticsearch-esamp5.service.chidc2.consul:9200 -p 5601:5601 docker-kibana5:dockerfile
 echo done.
